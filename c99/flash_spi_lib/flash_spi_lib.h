eb6dd400820f0c94c6182e975072ab410f591414
Nicolas Hasbun <nhasbun@gmail.com>
2018-03-09 15:11:48 -0300

    order to the library header

--- c99/flash_spi_lib/flash_spi_lib.h
+++ c99/flash_spi_lib/flash_spi_lib.h
@@ -7,19 +7,25 @@
 // Reduced library similar to stdio.h
 // alt_getchar(), alt_putchar(), alt_putstr(), alt_printf()
 
-// USEFUL MACRO to extract bit
-#define BIT(x,n) (((x) >> (n)) & 1)
 
-// *** MAIN INTERFACE ***
+// ****************************************************************************
+// *                              MAIN INTERFACE                              *
+// ****************************************************************************
 
-// main functions
+// -- MACROS --
+
+#define BIT(x,n) (((x) >> (n)) & 1) // USEFUL MACRO to extract bit
+
+// __ main functions __
+
 void read_id();
 void sector_erase(uint32_t add);
 void write_memory(uint32_t add, uint8_t value);
 uint8_t read_add(uint32_t add);
 void read_add_bulk(uint32_t add, uint32_t num_data, uint8_t * data);
 
-// status register related functions
+// __ status register related functions __
+
 void write_enable(); // ** write enable se deshabilita después de cada
 // funcion de escritura (revisar 8.2 write enable command en 8. Data Protection)
 void write_disable();
@@ -28,6 +34,6 @@ uint8_t read_status_register();
 void check_write_enable(bool value_target);
 void check_write_in_progress();
 
-// auxiliary functions
+// __ auxiliary functions __
 void split_32_to_8_bits(uint32_t number, uint8_t * pointer);
 
